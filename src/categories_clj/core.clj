d0022ecfacd20c9d1bb4df6791c256a2e8a3458b
Bruce C. Miller <bm3719@gmail.com>
2017-02-08 03:51:17 -0500

    Make small tweaks to core

--- src/categories_clj/core.clj
+++ src/categories_clj/core.clj
@@ -3,14 +3,17 @@
             [cats.monad.maybe :as maybe]))
 
 ;; Hi, I'm genderfluid.  Thanks to Tumblr and George Mason University, I've
-;; accumulated a list of the genders I fluidly fluctuate between.
-(def my-genders [;; Your gender can only be described as "you".
+;; accumulated a list of the genders I fluctuate between in n-dimensional
+;; fluidic gender space.
+(def my-genders [;; A gender that can only be described as "you".
                  "egogender"
                  ;; A gender that seems there but shallow, then upon inspection
                  ;; is naught but a wisp of the gender you thought it was.
                  "vapogender"
                  ;; Similar to a boy, but on a non-intersecting plane.
                  "proxvir"
+                 ;; A faintly shining, wavering gender.
+                 "glimragender"
                  ;; Being of all genders at once.
                  "omnigender"
                  ;; Having a feeling of a lack of a gender that feels neutral.
@@ -25,9 +28,9 @@
                  "hydrogender"])
 
 ;; Luckily, it's completely random which gender I am at any time.  The only
-;; problem is that I'm no gender at all about half the time.
+;; caveat is that I'm no gender at all about half the time.
 (defn query-gender []
   (rand-nth [(rand-nth my-genders) nil]))
 
-;; One problem though, I've discovered that I'm meta-phasing genderfluid to
-;; demifluid!  All my genders are now demi-genders!
+;; Oh no!  Now I've discovered that I'm meta-phasing genderfluid to demifluid!
+;; All my genders are now demi-genders!
